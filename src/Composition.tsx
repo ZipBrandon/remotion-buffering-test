b94becb3c220aa21bf87c1b0627a075da34defce
Brandon Lorenz <blorenz@gmail.com>
2024-05-08 09:37:21 -0400

    updates

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,4 +1,4 @@
-import {AbsoluteFill} from 'remotion';
+import {AbsoluteFill, Video} from 'remotion';
 import {Logo} from './Logo';
 import {Subtitle} from './Subtitle';
 import {Title} from './Title';
@@ -7,6 +7,7 @@ import {zColor} from '@remotion/zod-types';
 
 export const myCompSchema = z.object({
 	titleText: z.string(),
+	src: z.string(),
 	titleColor: zColor(),
 	logoColor: zColor(),
 });
@@ -15,14 +16,14 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
 	titleText: propOne,
 	titleColor: propTwo,
 	logoColor: propThree,
+	src,
 }) => {
 	return (
 		<AbsoluteFill className="bg-gray-100 items-center justify-center">
-			<div className="m-10" />
-			<Logo logoColor={propThree} />
-			<div className="m-3" />
-			<Title titleText={propOne} titleColor={propTwo} />
-			<Subtitle />
+		<Video
+			pauseWhenBuffering
+			src={src}
+		/>
 		</AbsoluteFill>
 	);
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,24 +1,93 @@
-import {Composition} from 'remotion';
-import {MyComposition, myCompSchema} from './Composition';
+import {MyComposition} from './Composition';
 import './style.css';
 
+import {Player, PlayerRef, RenderPlayPauseButton} from '@remotion/player';
+import {useCallback, useEffect, useRef, useState} from 'react';
+import {getVideoMetadata} from '@remotion/media-utils';
+
+const src = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4';
 export const RemotionRoot: React.FC = () => {
+
+ const playerRef = useRef<PlayerRef>(null);
+
+
+
+	const [buffering, setBuffering] = useState(false);
+
+	useEffect(() => {
+		const { current } = playerRef;
+		if (!current) {
+			return;
+		}
+
+		const onBuffering = () => {
+			setBuffering(true);
+			console.log("on buffer");
+		};
+		const onResume = () => {
+			setBuffering(false);
+			console.log("on resume");
+		};
+
+		current.addEventListener("waiting", onBuffering);
+		current.addEventListener("resume", onResume);
+		return () => {
+			current.removeEventListener("waiting", onBuffering);
+			current.removeEventListener("resume", onResume);
+		};
+	}, [playerRef, setBuffering]);
+
+	const [duration, setDuration] = useState(1);
+
+		useEffect(() => {
+		getVideoMetadata(src)
+			.then(({ durationInSeconds }) => {
+				setDuration(Math.round(durationInSeconds * 30));
+			})
+			.catch((err) => {
+				console.log(`Error fetching metadata: ${err}`);
+			});
+	}, [src]);
+	const renderPlayPauseButton: RenderPlayPauseButton = useCallback(
+		({ playing, isBuffering }) => {
+
+			console.log("playing", playing)
+			console.log("isBuffering", isBuffering)
+			if (playing && isBuffering) {
+				return <div>P</div>;
+			}
+
+			return null;
+		},
+		[],
+	);
+
 	return (
 		<>
-			<Composition
-				id="MyComp"
-				component={MyComposition}
-				durationInFrames={240}
-				fps={30}
-				width={1280}
-				height={720}
-				schema={myCompSchema}
-				defaultProps={{
-					titleText: 'Welcome to Remotion with Tailwind CSS',
-					titleColor: '#000000',
-					logoColor: '#00bfff',
-				}}
-			/>
+			<Player
+			component={MyComposition}
+			durationInFrames={
+			duration
+			}
+			ref={playerRef}
+			inputProps={{
+			titleText: 'Hello, world!',
+			titleColor: '#000',
+			logoColor: '#000',
+src
+			}}
+
+			controls={true}
+			alwaysShowControls={true}
+			compositionHeight={720}
+			compositionWidth={1280}
+			fps={30}
+
+			// renderPoster={renderPoster}
+			// showPosterWhenBuffering
+			renderPlayPauseButton={renderPlayPauseButton}
+		/>
+
 		</>
 	);
 };
